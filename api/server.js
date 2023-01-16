83ffa0b33c3d05c5177f110db7bf613803b4ca41
Praneeth <yennampraneeth@gmail.com>
2023-01-16 18:50:32 -0500

    errors in deleting tasks

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require("express"); //to handle api
 const mongoose = require("mongoose"); // to handle database
 const cors = require("cors");
 
-const app = express(); //to use express
+const app = express(); //to establish server
 
 app.use(express.json()); //content type of json application
 app.use(cors()); //protect from any cross origin errors
@@ -23,12 +23,12 @@ mongoose
 const Todo = require("./models/Todo"); //importing
 
 //if we make a request to "localhost/3001/todos" it find all todos from mongo data base and return to todos
-app.get("/todos", async (req, res) => {
+app.get('/todos', async (req, res) => {  //first paramter is the path(route),2nd function
   const todos = await Todo.find();
   res.json(todos);
-});
+});// GET() send data to be displayed when specific url is opened
 
-app.post("/todo/new", (req, res) => {
+app.post('/todo/new', (req, res) => {
   //creates a new collection in the database
   const todo = new Todo({
     text: req.body.text, 
@@ -39,7 +39,7 @@ app.post("/todo/new", (req, res) => {
 
 app.delete('/todo/delete/:id',async(req,res)=>{
   const result = await Todo.findByIdAndDelete(req.params.id)
-  res.json(result)
+  res.json({result})
 })
 
 app.get('/todo/complete/:id',async(req,res)=>{
@@ -49,7 +49,16 @@ app.get('/todo/complete/:id',async(req,res)=>{
   res.json(todo)
 })
 
+app.put('/todo/update/:id', async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.text = req.body.text;
+
+  todo.save();
+
+  res.json(todo);
+});
 
 
 
-app.listen(3001, () => console.log("Server started on port 3001"));
+app.listen(3001, () => console.log("Server started on port 3001")); //app listening at server port 3001
